ef833fe0b273c99bc94fd1a801978d26e894a7fe
Jerry Park <jooksan.park@samsung.com>
2014-08-20 09:13:34 +0900

    fixed encoding for python script

--- python/dumpdocker.py
+++ python/dumpdocker.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
 '''
 Created on 2014. 8. 19.
 
---
+++ python/gdb.cmd
@@ -0,0 +1,6 @@
+set height 0
+set logging file /tmp/d.gdb.tmp
+set logging on
+info shared
+set logging off
+quit
