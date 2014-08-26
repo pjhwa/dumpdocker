11f882e1bb91200e218f1b6b992b3d3eb239b250
earthsea <tirthica@hotmail.com>
2014-08-26 16:54:15 +0900

    2014.08.26 Some bug Fix

--- python/dumpdocker.py
+++ python/dumpdocker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python27
+#!/usr/bin/python
 # -*- coding:utf-8 -*-
 
 '''
@@ -6,67 +6,21 @@ Created on 2014. 8. 19.
 
 @author: earthsea
 '''
-from os.path import isfile,realpath,basename
+from os.path import isfile,islink,realpath,basename
 from optparse import OptionParser
 from time import localtime,strftime
 from socket import gethostname
 from subprocess import Popen,PIPE
 import sys,tarfile
 
-'''            
-def isthere(cmd,must):
-    try :
-        ret = Popen(["which",cmd],stdout=PIPE,stderr=open("/dev/null"))
-        realCmd = realpath(ret.stdout.readline().strip('\n'))
-        if not isdir(realCmd) :
-            return realCmd
-    except IOError as Ierr :
-        sys.stderr.write("%s not found : "%cmd+str(Ierr)+"\n")
-        if must :
-            sys.exit(2)
-
-def pkgthere(cmd):
-    try :
-        libList = []
-        ret = Popen(cmd,stdout=PIPE,stderr=open("/dev/null"))
-        for retline in ret.stdout.readlines() :
-            libList.append(retline.strip('\t').strip('\n'))
-        return libList
-    except IOError as Ierr :
-        sys.stderr.write("%s not found : "%cmd+str(Ierr)+"\n")
-   
-def findLib(whereldd,cmd):
-    try :
-        sharedList = []
-        ret = Popen([whereldd,cmd],stdout=PIPE,stderr=open("/dev/null"))
-        for retline in ret.stdout.readlines() :
-            line = retline.strip('\t').strip('\n')
-            if line.find("/") != -1 :
-                sharedList.append(realpath(line.split(' ')[-2]))
-        return sharedList
-    except IOError as Ierr :
-        sys.stderr.write("%s not found : "%cmd+str(Ierr)+"\n")
-
-def findLib(whereldd,cmd):
-    path = []
-    ret = exeComm('%s %s'%(whereldd,cmd),1)
-    for line in ret :
-        if line.find(" /") != -1 :
-            pathRaw = realpath(line.split()[-2])
-            if isfile(pathRaw) :
-                path.append(pathRaw)
-    return path
-
-
-'''
 def exeComm(cmd,std):
     try :
         retline = []
         cmdList = cmd.split()
         if std == 1 :
-            ret = Popen(cmdList,stdout=PIPE,stderr=open("/dev/null")).stout
+            ret = Popen(cmdList,stdout=PIPE,stderr=open("/dev/null")).stdout
         elif std == 2 :
-            ret = Popen(cmdList,stderr=PIPE,stdout=open("/dev/null")).sterr
+            ret = Popen(cmdList,stderr=PIPE,stdout=open("/dev/null")).stderr
         for x in ret.readlines() :
             retline.append(x.strip('\t').strip('\n'))
         return retline
@@ -80,7 +34,15 @@ def isthere(cmd):
         if isfile(line) :
             path = realpath(line)        
     return path
-         
+
+def makeSharedLibs(path):
+    retline = []
+    if isfile(path) :
+        retline.append(realpath(path))
+    if islink(path) :
+        retline.append(path)    
+    return retline
+     
 if __name__ == '__main__':
 
     DATE = strftime('%Y%m%d%H%M',localtime())
@@ -128,9 +90,8 @@ if __name__ == '__main__':
     retgdb = exeComm('%s %s %s -x gdb.cmd'%(whereCmd['gdb'],execfile,corefile),1)
     for line in retgdb :
         if line.find(" /") != -1 and line.startswith('0x') :
-            gdbRaw = realpath(line.split()[-1])
-            if isfile(gdbRaw) :
-                sharedlibs.append(gdbRaw)
+            gdbRaw = line.split()[-1]
+            sharedlibs.extend(makeSharedLibs(gdbRaw))    
     
     # 일반 명령어 라이브러리와 바이너리      
     util_cmdList = ["bash","ls","cp","grep","cat","diff","tail","head","vi","bc" ]
@@ -141,9 +102,7 @@ if __name__ == '__main__':
         for line in retldd :
             if line.find(" /") != -1 :
                 lddRaw = realpath(line.split()[-2])
-                if isfile(lddRaw) :
-                    sharedlibs.append(lddRaw)
-        #sharedlibs.extend(findLib(whereCmd['ldd'],whereCmd[cmd]))
+                sharedlibs.extend(makeSharedLibs(lddRaw))
         
     pkg_cmdList = ["dpkg","rpm"]
     for cmd in pkg_cmdList :
@@ -151,28 +110,25 @@ if __name__ == '__main__':
         if whereCmd[cmd] :
             if cmd == "dpkg" :
                 retpkg = exeComm('%s -L gdb'%whereCmd[cmd],1)
-                #pkgList = pkgthere([whereCmd[cmd],'-L','gdb'])
                 for line in retpkg :
-                    if isfile(line) :
-                        sharedlibs.append(realpath(line))
+                    sharedlibs.extend(makeSharedLibs(line))
             elif cmd == "rpm" :
                 retpkg =  exeComm('%s -qvl gdb'%whereCmd[cmd],1)
                 for line in retpkg :
                     lineRaw = line.split()[8]
-                    if isfile(lineRaw) :
-                        sharedlibs.append(lineRaw)                  
+                    sharedlibs.extend(makeSharedLibs(lineRaw))                 
     if not retpkg :
         sys.stderr.write("Couldn't find dpkg or RPM package.")
         sys.exit(2)
 
     # strace 처리 부분
-    #retstrace = Popen([whereCmd['strace'],'gdb','-h'],stdout=open('/dev/null'),stderr=PIPE).stderr.readlines()
     retstrace = exeComm('%s gdb -h'%whereCmd['strace'],2)
     for line in retstrace :
         if line.startswith('open("/') :
             retline = line.strip('\n')
             if (retline.find(' ENOENT ') == -1  and retline.find(' ENOTDIR ') == -1 and retline.find('/tmp/') == -1 and retline.find('/proc/') == -1 and retline.find('/dev/') == -1) :
-                sharedlibs.append(realpath(retline.split('"')[1]))
+                straceRaw = retline.split('"')[1]
+                sharedlibs.extend(makeSharedLibs(straceRaw))
      
     myset = set(sharedlibs)
 
